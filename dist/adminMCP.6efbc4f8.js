4686abf9374e44589023f2a1b84d83fb04504dae
RovoDev User <rovodev@example.com>
2025-07-23 02:11:45 +1000

    WIP: Save dist changes before merge

--- dist/adminMCP.6efbc4f8.js
+++ dist/adminMCP.6efbc4f8.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"4VwAv":[function(require,module,exports,__globalThis) {
+})({"lru0M":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 3000;
+var HMR_SERVER_PORT = 8080;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "439701173a9199ea";
 var HMR_USE_SSE = false;
@@ -28247,7 +28247,7 @@ $RefreshReg$(_c, "App");
   globalThis.$RefreshReg$ = prevRefreshReg;
   globalThis.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"dVPUn","@compiled/react/runtime":"eGy18","react":"jMk1U","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"7h6Pi","@atlaskit/icon/glyph/notification":"bi0Pb","@atlaskit/icon/glyph/question":"cVWXB","@atlaskit/navigation-system/top-nav-items":"hmaM5","@atlaskit/button/new":"jGwqc","@atlaskit/css-reset":"94cat","@atlaskit/tabs":"8E01h","@atlaskit/textfield":"iWm4W","@atlaskit/dynamic-table":"dbg8K","@atlaskit/modal-dialog":"1Upub","@atlaskit/icon/glyph/trash":"gfSKs","@atlaskit/icon/glyph/edit":"kgNUm","@atlaskit/logo":"4T6WN"}],"eGy18":[function(require,module,exports,__globalThis) {
+},{"react/jsx-dev-runtime":"dVPUn","@compiled/react/runtime":"eGy18","react":"jMk1U","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"7h6Pi","@atlaskit/icon/glyph/notification":"bi0Pb","@atlaskit/icon/glyph/question":"cVWXB","@atlaskit/navigation-system/top-nav-items":"hmaM5","@atlaskit/button/new":"jGwqc","@atlaskit/css-reset":"94cat","@atlaskit/tabs":"8E01h","@atlaskit/textfield":"iWm4W","@atlaskit/dynamic-table":"dbg8K","@atlaskit/modal-dialog":"1Upub","@atlaskit/icon/glyph/trash":"gfSKs","@atlaskit/logo":"4T6WN","@atlaskit/icon/glyph/edit":"kgNUm"}],"eGy18":[function(require,module,exports,__globalThis) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "CC", ()=>(0, _index.CC));
@@ -68639,62 +68639,7 @@ function _interopRequireDefault(e) {
 DeleteIcon.displayName = 'DeleteIcon';
 var _default = exports.default = DeleteIcon;
 
-},{"bd776f65d72659c3":"jMk1U","9b0e628cd3576015":"4oZdW"}],"kgNUm":[function(require,module,exports,__globalThis) {
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = void 0;
-var _react = _interopRequireDefault(require("e01d316691c7c1f5"));
-var _base = require("6162f754753b4fc8");
-var _edit = _interopRequireDefault(require("7b45ad4e726d6370"));
-function _interopRequireDefault(e) {
-    return e && e.__esModule ? e : {
-        default: e
-    };
-}
-const EditIcon = (props)=>/*#__PURE__*/ _react.default.createElement(_base.UNSAFE_IconFacade, Object.assign({
-        dangerouslySetGlyph: `<svg width="24" height="24" viewBox="0 0 24 24" role="presentation"><path fill="currentcolor" fill-rule="evenodd" d="M4.02 19.23a1 1 0 0 0 1.18 1.18l3.81-.78-4.21-4.21zm5.92-2.62-2.12-2.12L16.31 6l2.12 2.12zm9.9-9.9-2.12-2.12A2 2 0 0 0 16.31 4c-.51 0-1.02.2-1.41.59l-9.76 9.76 4.95 4.95 9.76-9.76a2 2 0 0 0 0-2.83"/></svg>`
-    }, props, {
-        newIcon: _edit.default
-    }));
-EditIcon.displayName = 'EditIcon';
-var _default = exports.default = EditIcon;
-
-},{"e01d316691c7c1f5":"jMk1U","6162f754753b4fc8":"iJoYh","7b45ad4e726d6370":"hZVeM"}],"hZVeM":[function(require,module,exports,__globalThis) {
-/**
- * THIS FILE WAS CREATED VIA CODEGEN DO NOT MODIFY {@see http://go/af-codegen}
- * @codegen <<SignedSource::d48c51235c7072e6617b637653c3f4fe>>
- * @codegenCommand yarn build:icon-glyphs
- */ "use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = void 0;
-var _react = _interopRequireDefault(require("c010bc75cf8a569b"));
-var _UNSAFE_baseNew = _interopRequireDefault(require("19a2b5528b952487"));
-function _interopRequireDefault(e) {
-    return e && e.__esModule ? e : {
-        default: e
-    };
-}
-/**
- * ⚠️ EXPERIMENTAL ⚠️ - New icons are in alpha - and subject to change or removal in future minor or patch releases.
- * Please reach out in #icon-contributions before using these in production.
- *
- * Icon: "Edit".
- * Category: single-purpose
- * Location: @atlaskit/icon
- * Usage guidance: Single purpose - Reserved for editing objects.
- * - [Examples](https://atlaskit.atlassian.com/packages/design-system/icon)
- * - [Code](https://atlaskit.atlassian.com/packages/design-system/icon/docs/custom-icons)
- */ const EditIcon = (props)=>/*#__PURE__*/ _react.default.createElement(_UNSAFE_baseNew.default, Object.assign({
-        dangerouslySetGlyph: `<path stroke="currentcolor" stroke-linejoin="round" stroke-width="1.5" d="M6 1.751H3c-.69 0-1.25.56-1.25 1.25v10c0 .69.56 1.25 1.25 1.25h10c.69 0 1.25-.56 1.25-1.25V10m-.75-5 1.116-1.116a1.25 1.25 0 0 0 0-1.768l-.732-.732a1.25 1.25 0 0 0-1.768 0L11 2.5M13.5 5 9.479 9.021c-.15.15-.336.26-.54.318l-3.189.911.911-3.189a1.25 1.25 0 0 1 .318-.54L11 2.5M13.5 5 11 2.5"/>`
-    }, props));
-EditIcon.displayName = 'EditIcon';
-var _default = exports.default = EditIcon;
-
-},{"c010bc75cf8a569b":"jMk1U","19a2b5528b952487":"4oZdW"}],"4T6WN":[function(require,module,exports,__globalThis) {
+},{"bd776f65d72659c3":"jMk1U","9b0e628cd3576015":"4oZdW"}],"4T6WN":[function(require,module,exports,__globalThis) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "AtlasIcon", ()=>(0, _atlas.AtlasIcon));
@@ -73800,6 +73745,61 @@ var NEW_ONLY_LOGOS = logoDocsSchema.filter(function(logo) {
     return logo.type === 'new';
 });
 
-},{"@babel/runtime/helpers/extends":"lKa0J","@babel/runtime/helpers/objectWithoutProperties":"j8lr0","react":"jMk1U","@atlaskit/platform-feature-flags":"3H8e6","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}]},["4VwAv","4dmnR"], "4dmnR", "parcelRequirede6b", {}, null, null, "http://localhost:3000")
+},{"@babel/runtime/helpers/extends":"lKa0J","@babel/runtime/helpers/objectWithoutProperties":"j8lr0","react":"jMk1U","@atlaskit/platform-feature-flags":"3H8e6","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}],"kgNUm":[function(require,module,exports,__globalThis) {
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("e01d316691c7c1f5"));
+var _base = require("6162f754753b4fc8");
+var _edit = _interopRequireDefault(require("7b45ad4e726d6370"));
+function _interopRequireDefault(e) {
+    return e && e.__esModule ? e : {
+        default: e
+    };
+}
+const EditIcon = (props)=>/*#__PURE__*/ _react.default.createElement(_base.UNSAFE_IconFacade, Object.assign({
+        dangerouslySetGlyph: `<svg width="24" height="24" viewBox="0 0 24 24" role="presentation"><path fill="currentcolor" fill-rule="evenodd" d="M4.02 19.23a1 1 0 0 0 1.18 1.18l3.81-.78-4.21-4.21zm5.92-2.62-2.12-2.12L16.31 6l2.12 2.12zm9.9-9.9-2.12-2.12A2 2 0 0 0 16.31 4c-.51 0-1.02.2-1.41.59l-9.76 9.76 4.95 4.95 9.76-9.76a2 2 0 0 0 0-2.83"/></svg>`
+    }, props, {
+        newIcon: _edit.default
+    }));
+EditIcon.displayName = 'EditIcon';
+var _default = exports.default = EditIcon;
+
+},{"e01d316691c7c1f5":"jMk1U","6162f754753b4fc8":"iJoYh","7b45ad4e726d6370":"hZVeM"}],"hZVeM":[function(require,module,exports,__globalThis) {
+/**
+ * THIS FILE WAS CREATED VIA CODEGEN DO NOT MODIFY {@see http://go/af-codegen}
+ * @codegen <<SignedSource::d48c51235c7072e6617b637653c3f4fe>>
+ * @codegenCommand yarn build:icon-glyphs
+ */ "use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("c010bc75cf8a569b"));
+var _UNSAFE_baseNew = _interopRequireDefault(require("19a2b5528b952487"));
+function _interopRequireDefault(e) {
+    return e && e.__esModule ? e : {
+        default: e
+    };
+}
+/**
+ * ⚠️ EXPERIMENTAL ⚠️ - New icons are in alpha - and subject to change or removal in future minor or patch releases.
+ * Please reach out in #icon-contributions before using these in production.
+ *
+ * Icon: "Edit".
+ * Category: single-purpose
+ * Location: @atlaskit/icon
+ * Usage guidance: Single purpose - Reserved for editing objects.
+ * - [Examples](https://atlaskit.atlassian.com/packages/design-system/icon)
+ * - [Code](https://atlaskit.atlassian.com/packages/design-system/icon/docs/custom-icons)
+ */ const EditIcon = (props)=>/*#__PURE__*/ _react.default.createElement(_UNSAFE_baseNew.default, Object.assign({
+        dangerouslySetGlyph: `<path stroke="currentcolor" stroke-linejoin="round" stroke-width="1.5" d="M6 1.751H3c-.69 0-1.25.56-1.25 1.25v10c0 .69.56 1.25 1.25 1.25h10c.69 0 1.25-.56 1.25-1.25V10m-.75-5 1.116-1.116a1.25 1.25 0 0 0 0-1.768l-.732-.732a1.25 1.25 0 0 0-1.768 0L11 2.5M13.5 5 9.479 9.021c-.15.15-.336.26-.54.318l-3.189.911.911-3.189a1.25 1.25 0 0 1 .318-.54L11 2.5M13.5 5 11 2.5"/>`
+    }, props));
+EditIcon.displayName = 'EditIcon';
+var _default = exports.default = EditIcon;
+
+},{"c010bc75cf8a569b":"jMk1U","19a2b5528b952487":"4oZdW"}]},["lru0M","4dmnR"], "4dmnR", "parcelRequirede6b", {}, null, null, "http://localhost:8080")
 
 //# sourceMappingURL=adminMCP.6efbc4f8.js.map
